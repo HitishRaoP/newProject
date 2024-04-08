c70e46ea8a48f38680968ebdd6be76b98e956c48
Hitish Rao P <hitishraop@gmail.com>
2024-04-08 07:31:02 +0530

    Api endpoint

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import { Dashboard } from "@/components/allProducts";
+"use client"
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
-  return (
-    <main>
-     
-    </main>
-  );
+    
+    return (
+        <main className="px-4 sm:pr-6 sm:pl-20 md:pl-[80px] md:pr-6">
+            <Input
+                placeholder="Search"
+                className="h-20 my-auto "
+                type="text"
+            />
+        </main>
+    );
 }
